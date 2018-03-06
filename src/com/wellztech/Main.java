f9f0b8e26ac50d1e2cec2b9ac850f3795fd9a586
dillonwells <dwells10@my.wcyc.edu>
2018-03-06 13:19:51 -0600

    First Working Commit

--- src/com/wellztech/Main.java
+++ src/com/wellztech/Main.java
@@ -1,8 +1,43 @@
 package com.wellztech;
 
+import java.util.*;
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    //csv Comma seperated values.
+
+        String fileName = "date_diff.csv";
+        File file = new File(fileName);
+
+        try {
+
+            Scanner inputStream = new Scanner(file);
+            while (inputStream.hasNext()){
+
+                String data = inputStream.nextLine();
+                System.out.println(data);
+
+
+            }
+
+            inputStream.close();
+
+        } catch (FileNotFoundException e){
+
+            e.printStackTrace();
+
+        }
+
+
+
+        Date today = new Date();
+        String line;
+        String Fields[];
+
+
+
+
     }
 }
